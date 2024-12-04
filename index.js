1551046eb078cca40c7e139ac81ccda374d88528
caleb <musyokacaleb5@gmail.com>
2024-12-04 09:37:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// Function to create thank you messages
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  // Function to count down and log numbers
+  function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Exporting the functions for testing
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
